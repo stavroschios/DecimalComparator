5a8f09de783534b558913786aa9758f04bc79a23
Aligned Ginger <15883219+stavroschios@users.noreply.github.com>
2019-04-14 17:31:51 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(areEqualByThreeDecimalPlaces(3.0, 3.0));
+        System.out.println(areEqualByThreeDecimalPlaces(3.0, 3.0)); // should say true
 
     }
 
